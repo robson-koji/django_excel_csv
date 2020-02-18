1a56f3ef634385294eb6c15830a0ce2866c1727f
Koji <rmoriya@fapesp.br>
2020-02-18 15:37:27 -0300

    create package, pip installer

---
+++ setup.py
@@ -0,0 +1,22 @@
+
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='django_excel_csv',
+     version='0.1',
+#     scripts=['django_excel_csv'] ,
+     author="BV FAPESP",
+     author_email="rmoriya@fapesp.br",
+     description="Django app to create csv file. Excel brazilian format and utf-8 compatible.",
+     long_description=long_description,
+     #long_description_content_type="text/markdown",
+     url="https://gitlab.com/bv_fapesp/django_excel_csv",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 2.7",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
+
